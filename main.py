3b43de51f07e302edcd47d51bfdf771c52ad27b8
celivian <serzh.ilyukin@bk.ru>
2023-10-31 15:32:40 +0300

    Добавил метод __init__ для запуска программы

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 class Organizer(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('untitled222.ui', self)
