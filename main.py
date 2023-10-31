ee287d1af5a9b6ee885782d5d591e897ee676496
celivian <serzh.ilyukin@bk.ru>
2023-10-31 16:23:40 +0300

    Добавил метод initUI

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ import sys
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
+
 class Organizer(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('main.ui', self)
+
+    def initUI(self):
+
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
