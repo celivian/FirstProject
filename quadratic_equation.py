368d753f96c28a866690ceea0655a0cc63d2edc4
celivian <serzh.ilyukin@bk.ru>
2023-10-31 18:49:24 +0300

    Создал и неполностью описал метод solution

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -2,3 +2,7 @@ class Quadratic_Equation():
     def __init__(self, type_of_equation, a, b, c):
         self.coefficients = [a, b, c]
         self.toe = type_of_equation
+
+    def solution(self):
+        if self.toe == 'ax² + bx + c = 0':
+            print(self.coefficients)
