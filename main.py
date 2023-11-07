92af32248c194e73a241c295e8ee70b75b158e3d
celivian <serzh.ilyukin@bk.ru>
2023-11-08 00:09:03 +0300

    Переписал код, заново добавил нужные импорты

--- main.py
+++ main.py
@@ -1,35 +1,6 @@
-import io
 import sys
-from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import uic, QtWidgets, QtCore, QtGui
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtGui import QIcon
 from quadratic_equation import *
-from input_error import *
-
-
-class Organizer(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi('main.ui', self)
-        self.setFixedSize(800, 600)
-        self.qequation = Quadratic_Equation(self)
-        self.changedtab()
-        self.initUI()
-
-    def initUI(self):
-        self.tabWidget.currentChanged.connect(self.changedtab)
-
-
-    def changedtab(self):
-        self.tabindex = self.tabWidget.currentIndex()
-        if self.tabindex == 0:
-            self.pushButton.clicked.connect(self.qequation.calculation)
-            self.comboBox.activated.connect(self.qequation.choice_equation)
-
-
-
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = Organizer()
-    ex.show()
-    sys.exit(app.exec_())
+from input_qe import *
