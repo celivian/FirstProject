25faa83a582352070c178a8a9224f5f82e4ca357
celivian <serzh.ilyukin@bk.ru>
2023-11-08 00:14:50 +0300

    Переписал код, добавил нужные импорты

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -1,23 +1,4 @@
-from input_error import *
-from main import *
+from input_qe import *
+import math as m
+from errors import *
 
-
-class Quadratic_Equation():
-    def __init__(self, type_of_equation, a, b, c):
-        self.coefficients = [a, b, c]
-        self.toe = type_of_equation
-
-    def solution(self, main):
-        if self.toe == 'ax² + bx + c = 0':
-            try:
-                if self.coefficients[1] and self.coefficients[2]:
-                    main.statusBar().hide()
-                    discr = int(self.coefficients[1]) ** 2 - 4 * int(self.coefficients[0]) * int(
-                        self.coefficients[2])
-                    main.label_6().setText('D = {self.coefficients[1]}² - 4 * {self.coefficients[0]} * {self.coefficients[2]} = {discr}')
-
-                else:
-                    raise Input_Error()
-            except Input_Error:
-                main.statusBar().show()
-                main.statusBar().showMessage("Заполните обязательные поля!")
