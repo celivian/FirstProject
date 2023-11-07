6f911b5e2f760afd4a6f1cd6f30ebbe867178a6a
celivian <serzh.ilyukin@bk.ru>
2023-11-08 00:11:38 +0300

    Переписал код, добавил класс InoutQE и описал метод __init__

---
+++ input_qe.py
@@ -0,0 +1,8 @@
+
+"""Это класс ввода информации. Здесь можно брать введеные пользователем данные"""
+
+class InputQE():
+    def __init__(self, main1, main2):
+        self.main = main1
+        self.qe = main2
+
