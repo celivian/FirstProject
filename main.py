2bf5e64936afa5f4e674db95d5882c0521f34cb4
celivian <serzh.ilyukin@bk.ru>
2023-11-08 00:09:40 +0300

    Добавил класс окна и описал метод __init__

--- main.py
+++ main.py
@@ -4,3 +4,14 @@ from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5.QtGui import QIcon
 from quadratic_equation import *
 from input_qe import *
+
+"""Это класс окна. Он запускает интерфейс программы"""
+
+class Organizer(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main.ui', self)
+        self.setFixedSize(800, 600)
+        self.setWindowIcon(QIcon("icon.ico"))
+        self.input = InputQE(self, Quadratic_Equation(self))
+        self.initUI(self)
