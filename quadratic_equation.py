71cf48b36bd87b20ca48637596a32769c0dde4d4
celivian <serzh.ilyukin@bk.ru>
2023-11-08 00:16:25 +0300

    Описал метод solution

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -9,3 +9,38 @@ class Quadratic_Equation():
         self.main = main
         self.inputqe = InputQE(self.main, self)
         self.inputqe.choice_equation()
+
+    """Метод solution определяет для какого уравнения нужно предоставить решение"""
+
+    def solution(self, coefs, toe):
+        self.coefficients = coefs
+        self.type_of_equation = toe
+        if not self.coefficients[0]:
+            self.coefficients[0] = '1'
+        if self.coefficients[0] == '-':
+            self.coefficients[0] = '-1'
+        try:
+            if self.coefficients[0] == '0' or self.coefficients[1] == '0' or self.coefficients[2] == '0' or \
+                    self.coefficients[0] == '-0' or self.coefficients[1] == '-0' or self.coefficients[2] == '-0' or \
+                    self.coefficients[0] == '+0' or self.coefficients[1] == '+0' or self.coefficients[2] == '+0':
+                raise Zero_Error()
+            if self.type_of_equation == 'ax² + bx + c = 0':
+                self.quadratic_equation1()
+            if self.type_of_equation == 'ax² + bx = 0':
+                self.quadratic_equation2()
+            if self.type_of_equation == 'ax² + c = 0':
+                self.quadratic_equation3()
+            if self.type_of_equation == 'ax² = 0':
+                self.quadratic_equation4()
+        except ValueError:
+            self.main.statusBar().showMessage("Ввeдите числа правильно!")
+            self.main.statusBar().show()
+            self.aftererror()
+        except Input_Error:
+            self.main.statusBar().showMessage("Заполните обязательные поля!")
+            self.main.statusBar().show()
+            self.aftererror()
+        except Zero_Error:
+            self.main.statusBar().showMessage("Ноль не может быть введен!")
+            self.main.statusBar().show()
+            self.aftererror()
