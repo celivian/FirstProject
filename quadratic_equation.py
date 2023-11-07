969a41db7e3b592f9269fe7f83fc1f7e8bf5f5b4
celivian <serzh.ilyukin@bk.ru>
2023-11-08 00:15:39 +0300

    Добавил класс Quadratic_Equation и описал метод __init__

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -2,3 +2,10 @@ from input_qe import *
 import math as m
 from errors import *
 
+"""Этот класс решает уравнения, исходя из полученных данных"""
+
+class Quadratic_Equation():
+    def __init__(self, main):
+        self.main = main
+        self.inputqe = InputQE(self.main, self)
+        self.inputqe.choice_equation()
