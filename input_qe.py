9eff1fea8f638be55e52f2beca8243b93bb21d07
celivian <serzh.ilyukin@bk.ru>
2023-11-08 00:12:28 +0300

    Описал метод choice_equation

--- input_qe.py
+++ input_qe.py
@@ -6,3 +6,58 @@ class InputQE():
         self.main = main1
         self.qe = main2
 
+    """Метод choice_equation отслеживает выбранное уравнение и в соответствии с ним меняет поле вывода информации"""
+
+    def choice_equation(self):
+        self.type_of_equation = self.main.comboBox.currentText()
+        if self.type_of_equation == 'ax² + bx + c = 0':
+            self.main.label_3.show()
+            self.main.coefb.show()
+            self.main.label_2.show()
+            self.main.coefc.show()
+            self.main.label_6.show()
+            self.main.label_7.show()
+            self.main.label_9.show()
+            self.main.label_10.show()
+            self.main.label_6.setText('D = b² - 4 * a * c = ?')
+            self.main.label_7.setText('D ? 0')
+            self.main.label_9.setText('X₁ = (-(b) - √D) / 2 * a = ?')
+            self.main.label_10.setText('X₂ = (-(b) + √D) / 2 * a = ?')
+            self.main.label_9.show()
+            self.main.label_10.show()
+        if self.type_of_equation == 'ax² + bx = 0':
+            self.main.label_3.show()
+            self.main.coefb.show()
+            self.main.label_2.hide()
+            self.main.coefc.hide()
+            self.main.label_6.hide()
+            self.main.label_7.hide()
+            self.main.label_9.setText('X₁ = ?')
+            self.main.label_10.setText('X₂ = -b / a = ?')
+            self.main.label_9.show()
+            self.main.label_10.show()
+        if self.type_of_equation == 'ax² + c = 0':
+            self.main.label_2.show()
+            self.main.coefc.show()
+            self.main.label_3.hide()
+            self.main.coefb.hide()
+            self.main.label_6.hide()
+            self.main.label_7.hide()
+            self.main.label_9.setText('X₁ = √(-с / a) = ?')
+            self.main.label_10.setText('X₂ = -√(-с / a) = ?')
+            self.main.label_9.show()
+            self.main.label_10.show()
+        if self.type_of_equation == 'ax² = 0':
+            self.main.label_2.hide()
+            self.main.coefc.hide()
+            self.main.label_3.hide()
+            self.main.coefb.hide()
+            self.main.label_6.hide()
+            self.main.label_7.hide()
+            self.main.label_10.setText('X = ?')
+            self.main.label_9.hide()
+        self.main.coefa.setText('')
+        self.main.coefb.setText('')
+        self.main.coefc.setText('')
+        self.main.label_8.setText('Количество корней: ?')
+        self.main.label_11.setText(f'Ответ: ?')
